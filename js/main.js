a21ed60e622fecd3882643a2395dbb01f69c8187
Егор Хрипко <62298485+4kouj1r0@users.noreply.github.com>
2021-02-24 16:47:02 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+   
+    
+$('.banner-section_slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section_slider-btn banner-section_slider-btnprev"><img src="/images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section_slider-btn banner-section_slider-btnnext"><img src="/images/arrow-right.svg" alt=""></button>',
+    
+});
+});
